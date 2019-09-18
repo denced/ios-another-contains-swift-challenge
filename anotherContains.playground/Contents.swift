da3aebb3d778c188fd1f94bc99a1b5f555216070
Denis Cedeno <denis.cedeno@gmail.com>
2019-09-17 21:30:21 -0400

    initial commit, challenge not completed

--- anotherContains.playground/Contents.swift
+++ anotherContains.playground/Contents.swift
@@ -1,8 +1,13 @@
 import Foundation
 
 extension String {
-    
+    func anotherContains(_ string: String) -> Bool{
+        for string in self.String{
+            if 
+        }
+    }
 }
+
 
 
 // test cases
@@ -10,3 +15,8 @@ print("Where is WaLdO".anotherContains("WALDO")) // true
 print("Where is WaLdO".anotherContains("where")) // true
 print("Where is WaLdO".anotherContains("is wA")) // true
 print("Where is WaLdO".anotherContains("nOPe"))  // false
+
+
+//Write your own version of Swift’s contains(_:) method on String class namedanotherContains().
+//Your implementation should be an extension on String and ignore letter case.
+//You obviously can’t use any form of the built-in contains(_:) method, but NSString has some really groovy methods for Finding Characters and Substrings like range(of:).
